fe98055e081b3a242a45f7893d948f7084ce390d
littlejohnnyzq <littlejohnnyzq@icloud.com>
2019-07-06 13:12:18 +0800

    1.2

--- files/page1/data.js
+++
@@ -1,7 +0,0 @@
-﻿$axure.loadCurrentPage(
-(function() {
-    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
-    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,[j],k,_(l,m,n,o,p,q,r,_(),s,_(t,u,v,w,x,_(y,z,A,B),C,null,D,w,E,w,F,G,H,null,I,J,K,L,M,N,O,J),P,_(),Q,_(R,_(S,T,U,[_(S,V,W,g,X,[_(Y,Z,S,ba,bb,[])])])),bc,_(bd,[])),be,_(),bf,_());}; 
-var b="url",c="page1.html",d="generationDate",e=new Date(1562362902703),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="variables",j="OnLoadVariable",k="page",l="packageId",m="798423f1513c44b6af7efe05b86533cc",n="type",o="Axure:Page",p="name",q="page1",r="notes",s="style",t="baseStyle",u="627587b6038d43cca051c114ac41ad32",v="pageAlignment",w="near",x="fill",y="fillType",z="solid",A="color",B=0xFFFFFFFF,C="image",D="imageHorizontalAlignment",E="imageVerticalAlignment",F="imageRepeat",G="auto",H="favicon",I="sketchFactor",J="0",K="colorStyle",L="appliedColor",M="fontName",N="Applied Font",O="borderWidth",P="adaptiveStyles",Q="interactionMap",R="onLoad",S="description",T="页面载入时 ",U="cases",V="Case 1",W="isNewIfGroup",X="actions",Y="action",Z="fadeWidget",ba="显示/隐藏元件",bb="objectsToFades",bc="diagram",bd="objects",be="masters",bf="objectPaths";
-return _creator();
-})());
